32a799dfe46a84be631fd5516239ce67cd59491f
Vinícius Souza <vinicius.sfs91@gmail.com>
2021-08-02 23:43:48 -0300

    primeiro commit

---
+++ src/Components/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+import { UserContext } from "../UserContext";
+
+const Header = () => {
+  const { data, userLogout } = React.useContext(UserContext);
+
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link to="/" className={styles.logo} aria-label="Dogs - Home">
+          <Dogs />
+        </Link>
+        {data ? (
+          <Link to="/conta" className={styles.login}>
+            {data.nome}
+            <button onClick={userLogout}>Sair</button>
+          </Link>
+        ) : (
+          <Link to="/login" className={styles.login}>
+            Login / Criar
+          </Link>
+        )}
+
+        
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/api.js
@@ -0,0 +1,55 @@
+export const API_URL = "https://dogsapi.origamid.dev/json";
+
+// Solicita o token do usuario após autenticação
+export function TOKEN_POST(body) {
+  return {
+    url: API_URL + "/jwt-auth/v1/token",
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    },
+  };
+}
+
+// Verifica se o token é valido
+export function TOKEN_VALIDATE_POST(token) {
+  return {
+    url: API_URL + "/jwt-auth/v1/token/validate",
+    options: {
+      method: "POST",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    },
+  };
+}
+
+// Solicita os dados do usuário após informar o token
+export function USER_GET(token) {
+  return {
+    url: API_URL + "/api/user",
+    options: {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    },
+  };
+}
+
+// Cadastrar um usuario
+export function USER_POST(body) {
+  return {
+    url: API_URL + "/api/user",
+    options: {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    },
+  };
+} 
